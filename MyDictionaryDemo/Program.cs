3acf1bbc670ffda4b42f80af82c07b3cd30d9cfa
Buğra Arslan <bgra.arsln@hotmail.com>
2021-02-02 19:15:12 +0300

    Add project files.

---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Buğra");
+            isimler.Add("Mehmet");
+            isimler.Add("Enes");
+        }
+    }
+}
